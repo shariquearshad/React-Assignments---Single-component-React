4ab2c4cc4961eca0e8451634e6ab8976ef5524b6
shariquearshad <shariquearshad@gmail.com>
2020-12-10 19:34:26 +0530

    changed function react change

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
-const App = () => {
+export default function App() {
   return (
-    <div id="main"></div>
-  )
+    <div className="App" id="root">
+      <p>I am learning React. My life is getting better. </p>
+    </div>
+  );
 }
-
-
-export default App;
